7080270930a0268a11d0eb7570ee3d4ada732da4
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-20 02:03:13 +0000

    XML comment

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Declare the class Program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Invoke Main method to print "Hello LP1!" and "Bye world!"
+        /// </summary>
+        /// <param name="args">string</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
