fceb75345423decc405c75b07301cc186fbe0a1f
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-21 00:47:38 +0000

    Add comments

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,14 +7,21 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
+            //Show insert integer line for the user
             Console.WriteLine("Insira um número inteiro:");
+            //Declare and assign input variable
             string input_int = Console.ReadLine();
+            //Convert input into int
             int conv_int = Convert.ToInt32(input_int);
 
+            //Show insert real num line for the user
             Console.WriteLine("Insira um número real:");
+            //Declare and assign input variable
             string input_real = Console.ReadLine();
+            //Convert input into double
             double conv_real = Convert.ToSingle(input_real);
 
+            //Sentence with the sum of int and double variables 
             Console.WriteLine($"A soma entre {conv_int} e {conv_real} é {conv_int + conv_real}.");
         }
     }
