bbdd1592ec45722d9d0bc8f1040e53c4cab1f233
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-21 00:38:26 +0000

    Add input intenger

---
+++ AskForInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            string input_int = Console.ReadLine();
+            int conv_int = Convert.ToInt32(input_int);
+
+        }
+    }
+}
