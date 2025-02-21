879c710c5dfe7d3571729f310733b676999f9861
Rafael Canhoto <rafgoncan99@gmail.com>
2025-02-21 18:00:30 +0000

    user input

---
+++ Smiley/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //User input
+            Console.WriteLine("Insere um número interio em formato hexadecimal:");
+            string hexadecimal = Console.ReadLine();
+
+            //1F39C
+            //263A
+            //1F552
+        }
+    }
+}
